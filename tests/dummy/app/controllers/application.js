8150d61e791bbbadf472dc579347937925eef034
Dan Sudol <dansudol@yahoo.com>
2016-12-23 14:55:27 -0500

    bower touch ups

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
   leftItems: Array(100).fill().map((_, i)=>i),
 
   rightItems: Array(100).fill().map((_, i)=> {
-    return { a: i.toString(2), b: i.toString(16), c: i.toString(24), d: i.toString(36) }
+    return { a: i.toString(2), b: i.toString(16), c: i.toString(24), d: i.toString(36) };
   }),
   rightHeaderComponent: 'right-header-row',
   rightSideComponent: 'right-row',
